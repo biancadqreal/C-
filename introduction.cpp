2f957f57963f97b43a1ec8165eac9a7b1888c22c
Bianca Garcia <69773280+biancadqreal@users.noreply.github.com>
2022-09-26 08:36:24 -0300

    Create introduction.cpp

---
+++ introduction.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //Biblioteca padrão da linguagem C++
+
+using namespace std; //Evita de colocar std:: na função, nem sempre considerado uma boa prática
+
+int main() //Função principal
+{
+    cout<<"Hello World" << endl;//Semelhante ao comando print, escreve "Hello World" na tela
+    //Os sinais de menor são utilizados para representar saída
+    //endl; Demonstra quebra de linha ao programa
+
+    return 0; //Mensagem que informa que o programa terminou de forma normal
+}
